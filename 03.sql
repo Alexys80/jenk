601c97a656467e943f107185ac4d1a3675b62a88
Alexys80 <alexys80@gmail.com>
2016-02-05 16:26:00 +0300

    Create 03.sql

---
+++ 03.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE IF NOT EXISTS testdb;
+USE testdb;
+DROP TABLE IF EXISTS table3;
+CREATE TABLE table3 (id int(5) NOT NULL AUTO_INCREMENT, text varchar(250) DEFAULT NULL, PRIMARY KEY(id));
+INSERT INTO table3 (text) VALUES ("1st row");
+INSERT INTO table3 (text) VALUES ("2nd row");
+INSERT INTO table3 (text) VALUES ("3rd row");
+INSERT INTO table3 (text) VALUES ("4th row");
+INSERT INTO table3 (text) VALUES ("5th row");
+INSERT INTO table3 (text) VALUES ("6th row");
+commit;
